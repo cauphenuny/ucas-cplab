f7fa968410b12eb7cc1ab1462cf4ca791c960cfd
cauphenuny <starseekist@gmail.com>
2026-04-02 13:21:47 +0800

    feat: op evaluation & assignment in VM

---
+++ src/backend/ir/vm/assign.cpp
@@ -0,0 +1,76 @@
+#include "vm.h"
+#include <cstddef>
+
+namespace ir::vm {
+
+void VirtualMachine::assign(const adt::Primitive& dest_type, std::byte* dest,
+                            const adt::Primitive& src_type, const std::byte* src) const {
+    Match(dest_type, src_type)([&](auto dest_prim, auto src_prim) {
+        if constexpr (std::is_same_v<decltype(dest_prim), decltype(src_prim)>) {
+            memcpy(dest, src, layout.size_of(dest_type));
+        } else {
+            throw CompilerError(fmt::format("Cannot assign {} to {}", src_type, dest_type));
+        }
+    });
+}
+
+void VirtualMachine::assign(const adt::Sum& dest_type, std::byte* dest, const ir::Type& src_type,
+                            const std::byte* src) const {
+    if (src_type.is<adt::Sum>()) {
+        auto [src_tag, src_data] = as_sumtype(src);
+        auto& src_type = dest_type.type_of(src_tag);
+        auto dest_index = dest_type.index_of(src_type);
+        auto [dest_tag, dest_data] = as_sumtype(dest);
+        dest_tag = dest_index;
+        assign(src_type, dest_data, src_type, src_data);
+    } else {
+        auto index = dest_type.index_of(src_type);
+        auto [tag, data] = as_sumtype(dest);
+        tag = index;
+        assign(dest_type, data, src_type, src);
+    }
+}
+
+void VirtualMachine::assign(const adt::Array& dest_type, std::byte* dest, const adt::Array& src_type,
+                            const std::byte* src) const {
+    if (dest_type.size < src_type.size) {
+        throw CompilerError(fmt::format("Cannot assign array of size {} to array of size {}",
+                                        src_type.size, dest_type.size));
+    }
+    size_t i = 0;
+    auto src_view = as_array(src, layout.size_of(src_type.elem));
+    auto dest_view = as_array(dest, layout.size_of(dest_type.elem));
+    for (i = 0; i < src_type.size; i++) {
+        assign(dest_type.elem, dest_view[i], src_type.elem, src_view[i]);
+    }
+    for (; i < dest_type.size; i++) {
+        memset(dest_view[i], 0, layout.size_of(dest_type.elem));
+    }
+}
+
+void VirtualMachine::assign(const adt::Product& dest_type, std::byte* dest, const adt::Product& src_type,
+                            const std::byte* src) const {
+    if (dest_type.items().size() != src_type.items().size()) {
+        throw CompilerError(fmt::format("Cannot assign {} to {}", src_type, dest_type));
+    }
+    size_t offset = 0;
+    for (size_t i = 0; i < dest_type.items().size(); i++) {
+        auto& dest_item = dest_type.items()[i];
+        auto& src_item = src_type.items()[i];
+        assign(dest_item, (std::byte*)dest + offset, src_item, (const std::byte*)src + offset);
+        offset += layout.size_of(dest_item);
+    }
+}
+
+void VirtualMachine::assign(View dest, const View& src) const {
+    if (dest.type.is<adt::Sum>()) {
+        auto& sum_type = dest.type.as<adt::Sum>();
+        assign(sum_type, dest.data, src.type, src.data);
+    } else {
+        Match{dest.type.var(), src.type.var()}([&](const auto& dest_type, const auto& src_type) {
+            assign(dest_type, dest.data, src_type, src.data);
+        });
+    }
+}
+
+}  // namespace ir::vm
---
+++ src/backend/ir/vm/vm.h
@@ -0,0 +1,195 @@
+#include "../ir.hpp"
+#include "backend/ir/op.hpp"
+#include "backend/ir/type.hpp"
+#include "frontend/ast/analysis/semantic_ast.h"
+#include "size.hpp"
+#include "utils/error.hpp"
+#include "view.hpp"
+
+#include <cstddef>
+#include <functional>
+#include <type_traits>
+#include <unordered_map>
+#include <variant>
+
+namespace ir::vm {
+
+class VirtualMachine {
+
+    template <template <typename> class Op>
+    void eval_binary(View& dest, const View& lhs, const View& rhs) const {
+        match(dest.type.as<adt::Primitive>(), [&](auto value) {
+            using type = typename decltype(value)::type;
+            if constexpr (std::is_floating_point_v<type> &&
+                          std::is_same_v<Op<type>, std::modulus<type>>) {
+                throw CompilerError("Cannot apply modulus operator to floating point");
+            } else {
+                *(type*)dest.data = Op<type>{}(*(type*)lhs.data, *(type*)rhs.data);
+            }
+        });
+    }
+
+    template <template <typename> class Op>
+    void eval_unary(View& dest, const View& operand) const {
+        match(dest.type.as<adt::Primitive>(), [&](auto value) {
+            using type = typename decltype(value)::type;
+            *(type*)dest.data = Op<type>{}(*(type*)operand.data);
+        });
+    }
+
+    template <template <typename> class Op>
+    void eval_comparison(View& dest, const View& lhs, const View& rhs) const {
+        match(lhs.type.as<adt::Primitive>(), [&](auto value) {
+            using type = typename decltype(value)::type;
+            *(bool*)dest.data = Op<type>{}(*(type*)lhs.data, *(type*)rhs.data);
+        });
+    }
+
+    void eval(InstOp op, View& dest, const View& lhs, const View& rhs) const {
+        auto it = binary_ops.find(op);
+        if (it != binary_ops.end()) {
+            it->second(dest, lhs, rhs);
+        } else {
+            throw CompilerError(
+                fmt::format("Unsupported binary operation: {}", static_cast<int>(op)));
+        }
+    }
+
+    void eval(UnaryInstOp op, View& dest, const View& operand) const {
+        auto it = unary_ops.find(op);
+        if (it != unary_ops.end()) {
+            it->second(dest, operand);
+        } else {
+            throw CompilerError(
+                fmt::format("Unsupported unary operation: {}", static_cast<int>(op)));
+        }
+    }
+
+    template <typename T1, typename T2>
+    void assign(const T1& dest_type, std::byte* dest, const T2& src_type,
+                const std::byte* src) const {
+        throw CompilerError(fmt::format("Cannot assign {} to {}", src_type, dest_type));
+    }
+
+    void assign(const adt::Primitive& dest_type, std::byte* dest, const adt::Primitive& src_type,
+                const std::byte* src) const;
+
+    void assign(const adt::Sum& dest_type, std::byte* dest, const ir::Type& src_type,
+                const std::byte* src) const;
+
+    void assign(const adt::Array& dest_type, std::byte* dest, const adt::Array& src_type,
+                const std::byte* src) const;
+
+    void assign(const adt::Product& dest_type, std::byte* dest, const adt::Product& src_type,
+                const std::byte* src) const;
+
+    void assign(View dest, const View& src) const;
+
+    void execute(const BinaryInst& inst, const View& lhs, const View& rhs, View& ret) const;
+    void execute(const UnaryInst& inst, const View& operand, View& ret) const;
+    void execute(const CallInst& inst, const std::vector<View>& srcs, View& ret) const;
+    void execute(const Func& func, const View& args, View& ret) const;
+
+    DataLayout layout{sizeof(int), sizeof(float), sizeof(double), sizeof(std::byte*)};
+
+    using BinaryOpFunc = std::function<void(View dest, const View& lhs, const View& rhs)>;
+    std::unordered_map<InstOp, BinaryOpFunc> binary_ops;
+
+    using UnaryOpFunc = std::function<void(View dest, const View& operand)>;
+    std::unordered_map<UnaryInstOp, UnaryOpFunc> unary_ops;
+
+    const ast::SemanticAST* ast;
+    const Program* program;
+
+    std::istream& input;
+    std::ostream& output;
+
+public:
+    void execute(const Program& program);
+
+    VirtualMachine(std::istream& input, std::ostream& output) : input(input), output(output) {
+        unary_ops[UnaryInstOp::MOV] = [this](const View& dest, const View& operand) {
+            std::memcpy(dest.data, operand.data, layout.size_of(dest.type));
+        };
+        unary_ops[UnaryInstOp::NOT] = [this](View dest, const View& operand) {
+            eval_unary<std::logical_not>(dest, operand);
+        };
+        unary_ops[UnaryInstOp::NEG] = [this](View dest, const View& operand) {
+            eval_unary<std::negate>(dest, operand);
+        };
+
+        binary_ops[InstOp::ADD] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_binary<std::plus>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::SUB] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_binary<std::minus>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::MUL] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_binary<std::multiplies>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::DIV] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_binary<std::divides>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::MOD] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_binary<std::modulus>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::LT] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_comparison<std::less>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::GT] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_comparison<std::greater>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::LEQ] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_comparison<std::less_equal>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::GEQ] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_comparison<std::greater_equal>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::EQ] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_comparison<std::equal_to>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::NEQ] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_comparison<std::not_equal_to>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::AND] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_binary<std::logical_and>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::OR] = [this](View dest, const View& lhs, const View& rhs) {
+            eval_binary<std::logical_or>(dest, lhs, rhs);
+        };
+        binary_ops[InstOp::LOAD] = [this](View dest, const View& lhs, const View& rhs) {
+            // NOTE: lhs: pointer, rhs: offset
+            using namespace adt;
+            if (!rhs.type.is<Primitive>() ||
+                !std::holds_alternative<Int>(rhs.type.as<Primitive>())) {
+                throw CompilerError(fmt::format("Offset must be an integer, but got {}", rhs.type));
+            }
+            if (!lhs.type.is<Pointer>() && !lhs.type.is<Array>()) {
+                throw CompilerError(
+                    fmt::format("Expected pointer or array type, but got {}", lhs.type));
+            }
+            auto offset = *(int*)rhs.data;
+            auto elem_type =
+                lhs.type.is<Pointer>() ? lhs.type.as<Pointer>().elem : lhs.type.as<Array>().elem;
+            dest.data = lhs.data + offset * layout.size_of(elem_type);
+        };
+        binary_ops[InstOp::STORE] = [this](View dest, const View& lhs, const View& rhs) {
+            // NOTE: dest: pointer, lhs: offset, rhs: value
+            using namespace adt;
+            if (!lhs.type.is<Primitive>() ||
+                !std::holds_alternative<Int>(lhs.type.as<Primitive>())) {
+                throw CompilerError(fmt::format("Offset must be an integer, but got {}", lhs.type));
+            }
+            if (!dest.type.is<Pointer>() && !dest.type.is<Array>()) {
+                throw CompilerError(
+                    fmt::format("Expected pointer or array type, but got {}", dest.type));
+            }
+            auto offset = *(int*)lhs.data;
+            auto elem_type =
+                dest.type.is<Pointer>() ? dest.type.as<Pointer>().elem : dest.type.as<Array>().elem;
+            assign(elem_type, dest.data + offset * layout.size_of(elem_type), rhs.type, rhs.data);
+        };
+    }
+};
+
+}  // namespace ir::vm
