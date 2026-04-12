f38b836edf707a70ec476895d19ec8cee276bd1b
cauphenuny <starseekist@gmail.com>
2026-04-12 18:13:37 +0800

    docs(ir): add explanation of algorithm solving dominance tree

--- src/backend/ir/optim/domtree.hpp
+++ src/backend/ir/optim/domtree.hpp
@@ -15,12 +15,26 @@ struct DominanceTree {
         return it->second;
     }
 
+    // clang-format off
+
+    /// NOTE: the idom is the dominator which has the largest dominator set (closest to the block)
+
+    // proof:
+    //   - lemma: if u dom w and v dom w, then u dom v or v dom u.
+    //     proof: if not, then there exists a path from entry to w that goes through u but not v, contradicts with v dom w.
+    //   - lemma: any differenct dominator do not has same-sized dominator set.
+    //     proof: consider u dom w, v dom w, then u dom v or v dom u, contradicts with |dom(u)| == |dom(v)|
+    //   since each dominator has different-sized dominator set, the immediate dominator must be the one with largest dominator set.
+
+    // clang-format on
+
     DominanceTree(const DataFlow<flows::Dominance>& dom_flow) {
         auto& func = dom_flow.cfg.func;
         auto& dom = dom_flow.out;
         for (const auto& block_box : func.blocks()) {
             auto block = block_box.get();
-            if (block == func.entrance()) idom_map[block] = nullptr;  // entry block has no dominator
+            if (block == func.entrance())
+                idom_map[block] = nullptr;  // entry block has no dominator
             const Block* idom_block = nullptr;
             for (auto dom_block : dom.at(block)) {
                 if (dom_block == block) continue;
