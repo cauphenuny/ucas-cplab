75bb5299258bfb9a3dab768930f4f43ed8f24789
cauphenuny <starseekist@gmail.com>
2026-04-20 18:18:30 +0800

    fix(ir optim): check all vars(def, use) in DeadAlloc, not just used vars

--- src/backend/ir/optim/dead_alloc.hpp
+++ src/backend/ir/optim/dead_alloc.hpp
@@ -19,20 +19,20 @@ struct DeadAllocElimination : Pass {
         }
         bool changed = false;
         for (auto& func : prog.getFuncs()) {
-            std::unordered_map<const Alloc*, bool> used;
-            for (auto use_var : analysis::utils::used_vars(*func)) {
-                if (auto named = std::get_if<NamedValue>(use_var); named) {
+            std::unordered_map<const Alloc*, bool> referenced;
+            for (auto var : analysis::utils::vars(*func)) {
+                if (auto named = std::get_if<NamedValue>(var); named) {
                     if (auto alloc = std::get_if<const Alloc*>(&named->def); alloc) {
-                        used[*alloc] = true;
+                        referenced[*alloc] = true;
                     }
                 }
-                if (auto ssa = std::get_if<SSAValue>(use_var); ssa) {
-                    used[ssa->def] = true;
+                if (auto ssa = std::get_if<SSAValue>(var); ssa) {
+                    referenced[ssa->def] = true;
                 }
             }
             std::vector<std::unique_ptr<Alloc>> kept_locals;
             for (auto& alloc : func->locals()) {
-                if (used.count(alloc.get())) {
+                if (referenced.count(alloc.get())) {
                     kept_locals.push_back(std::move(alloc));
                 } else {
                     changed = true;
